7036d7039732629f50c2a513b011af3a530b5416
gitluky <jluk87@gmail.com>
2017-09-08 02:06:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function dec(n){
+  return n--
+}
+
+function inc(n){
+  return n++
+}
+
+makeInt(n, 10){
+  return parseInt(n)
+}
+
+preserveDecimal(n){
+  parseFloat(n)
+}
+
+
